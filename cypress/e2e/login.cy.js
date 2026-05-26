448605c42512f0d7cb7b5652a07103e82f11685b
Hadis <it.karimzade@gmail.com>
2026-05-26 05:56:49 +0200

    Todo App Test automatisiert

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Todo App Test', () => {
+
+  it('Benutzer kann neue Aufgaben hinzufügen', () => {
+
+    cy.visit('https://todomvc.com/examples/react/dist/')
+
+    cy.get('.new-todo')
+      .should('be.visible')
+      .type('QA Automation lernen{enter}')
+
+    cy.get('.todo-list')
+      .should('contain', 'QA Automation lernen')
+
   })
+
 })
